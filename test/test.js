826d5629a614bc75736f430ef385fdf3125d55f6
Emil 'Skeen' Madsen <sovende@gmail.com>
2017-02-15 22:31:09 +0100

    Use assert.equal over assert

--- test/test.js
+++ test/test.js
@@ -26,32 +26,48 @@ describe('CuraEngineInternal', function()
         {
             var engine = require("./../build/CuraEngineInternal.js");
 
-            // Collect stdout and stderr
+            // Collect stdout
             var stdout = "";
             engine.emitter.on("stdout", function(text)
             {
                 stdout += text + "\n";
             });
-
+            // ... and stderr
             var stderr = "";
             engine.emitter.on("stderr", function(text)
             {
                 stderr += text + "\n";
             });
-
             // Run engine without arguments
             engine.main([]);
-
-            //console.log("DONE");
-            //console.log(stdout);
-            //console.log(stderr);
-
-            // Check that it prints stderr header
-            assert(stderr.startsWith("Cura_SteamEngine version DEV_WEB"),
-                    "Expected stderr to have Cura header");
-         
-
-            // TODO: Check invariants about stdout
+            
+            // Check stderr
+            //-------------
+            var stderr_arr = stderr.split('\n');
+            // Check that we've got version string
+            assert.equal(stderr_arr[0],
+                    "Cura_SteamEngine version DEV_WEB",
+                    "Expected stderr to have version header");
+            // Check that we've got copyright string
+            assert.equal(stderr_arr[1],
+                    "Copyright (C) 2014 David Braam",
+                    "Expected stderr to have copyright header");
+            // Check that we've got license string
+            assert.equal(stderr_arr[3],
+                    "This program is free software: you can redistribute it and/or modify",
+                    "Expected stderr to have license header");
+            // Check that no configuration is used
+            assert.equal(stderr_arr[15],
+                    "Default config \'default.cfg\' not used",
+                    "Expected stderr to have configuration line");
+            
+            // Check stdout
+            //-------------
+            var stdout_arr = stdout.split('\n');
+            // Check that we've got some gcodes
+            assert.deepEqual([stdout_arr[0], stdout_arr[5].substr(0,3), stdout_arr[9].substr(0,3)],
+                    ['M107', 'G91', 'M84'],
+                    "Expected stdout to have gcode header");
         });
 
         it('should output gcode header when run without args', function()
