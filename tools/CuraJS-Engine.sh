be5195a117d94c80e95b6d2845103bfddaa14df6
Emil 'Skeen' Madsen <sovende@gmail.com>
2017-02-15 23:06:40 +0100

    Travis can't lookup github releases without login

--- tools/CuraJS-Engine.sh
+++ tools/CuraJS-Engine.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Pull the latest engine release
-curl -s https://api.github.com/repos/Skeen/CuraJS-Engine/releases/latest | grep "browser_download_url" | sed 's#.*: "\(.*\)"#\1#g' | xargs curl -L
+#curl -s https://api.github.com/repos/Skeen/CuraJS-Engine/releases/latest | grep "browser_download_url" | sed 's#.*: "\(.*\)"#\1#g' | xargs curl -L
+
+curl -L https://github.com/Skeen/CuraJS-Engine/releases/download/TravisReleaseTest4/CuraEngine.js
+
